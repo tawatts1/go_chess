11c1c7d5b0a8235ddfe9788c7073612b95358cc5
Thaddeus Watts <tedawatts@gmail.com>
2025-01-03 10:44:12 -0700

    Added board and images

--- create_board/lib/main.dart
+++ create_board/lib/main.dart
@@ -1,109 +1,195 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'dart:async';
+import 'package:provider/provider.dart';
 
-import 'package:go_chess/go_chess.dart' as go_chess;
+const Space = '0';
+
+const BlackPawn = 'p';
+const BlackKnight = 'n';
+const BlackBishop = 'b';
+const BlackRookNC = 'r';
+const BlackQueen = 'q';
+const BlackKing = 'k';
+const BlackRookC = 'o';
+const BlackPawnEP = 'a';
+
+const WhitePawn =  'P';
+const WhiteKnight =  'N';
+const WhiteBishop =  'B';
+const WhiteRookNC =  'R';
+const WhiteQueen =  'Q';
+const WhiteKing =  'K';
+const WhiteRookC =  'O';
+const WhitePawnEP =  'A';
+
+const imageMap = {
+  BlackPawn: 'bp.png',
+  BlackKnight: 'bn.png',
+  BlackBishop: 'bb.png',
+  BlackRookNC: 'br.png',
+  BlackQueen: 'bq.png',
+  BlackKing: 'bk.png',
+  BlackRookC: 'br.png',
+  BlackPawnEP: 'bp.png',
+  WhitePawn: 'wp.png',
+  WhiteKnight: 'wn.png',
+  WhiteBishop: 'wb.png',
+  WhiteRookNC: 'wr.png',
+  WhiteQueen: 'wq.png',
+  WhiteKing: 'wk.png',
+  WhiteRookC: 'wr.png',
+  WhitePawnEP: 'wp.png'
+};
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  late int sumResult;
-  late Future<int> longSumResult;
-  final myController1 = TextEditingController();
-  final myController2 = TextEditingController();
-  @override
-  void dispose() {
-    myController1.dispose();
-    myController2.dispose();
-    super.dispose();
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    sumResult = go_chess.sum(0, 0);
-    longSumResult = go_chess.longSum(0, 0);
-  }
-
-
-
-  void _doCalc() {
-    setState(() {
-      var n1 = int.parse(myController1.text);
-      var n2 = int.parse(myController2.text);
-      longSumResult = Future<int>(() { return go_chess.longSum(n1, n2);});
-      sumResult = go_chess.sum(n1,n2);
-      //longSumResult = 
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    const textStyle = TextStyle(fontSize: 25);
-    const spacerSmall = SizedBox(height: 10);
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Native Packages'),
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         ),
-        body: SingleChildScrollView(
-          child: Container(
-            padding: const EdgeInsets.all(10),
-            child: Column(
-              children: [
-                const Text(
-                  'This calls a native function through FFI that is shipped as source in the package. '
-                  'The native code is built as part of the Flutter Runner build.',
-                  style: textStyle,
-                  textAlign: TextAlign.center,
-                ),
-                spacerSmall,
-                Text('First Number:'),
-                TextField(
-                  controller: myController1,
-                ),
-                Text('Second number: '),
-                TextField(
-                  controller: myController2,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    _doCalc();
-                    print('test $sumResult');
-                  },
-                  child: Text('do calculation'),
-                ),
-                Text(
-                  'The sum is $sumResult',
-                  style: textStyle,
-                  textAlign: TextAlign.center,
-                ),
-                spacerSmall,
-                FutureBuilder<int>(
-                  future: longSumResult,
-                  builder: (BuildContext context, AsyncSnapshot<int> value) {
-                    final displayValue =
-                        (value.hasData) ? value.data : 'loading';
-                    return Text(
-                      'The longSum is $displayValue',
-                      style: textStyle,
-                      textAlign: TextAlign.center,
-                    );
-                  },
-                ),
-              ],
-            ),
-          ),
-        ),
+        home: MyHomePage(),
       ),
     );
   }
 }
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+  var white = Color.fromARGB(255, 241, 189, 129);
+  var black = const Color.fromARGB(255, 99, 46, 11);
+  List<List<String>> board = [
+    ['o', 'n', 'b', 'k', 'q', '0', '0', 'o',],
+    ['p', 'p', 'p', 'p', 'p', '0', 'p', 'p',],
+    ['0', '0', '0', '0', '0', '0', '0', '0',],
+    ['0', '0', '0', '0', '0', '0', '0', '0',],
+    ['0', '0', '0', '0', '0', '0', '0', '0',],
+    ['0', '0', '0', '0', '0', '0', '0', '0',],
+    ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P',],
+    ['O', 'N', 'B', 'K', 'Q', '0', '0', 'O',],
+  ];
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    return Scaffold(
+      body: Column( 
+        //mainAxisAlignment: MainAxisAlignment.center,  
+        children: [
+          Text(appState.current.asLowerCase),
+          ElevatedButton(
+            onPressed: () {
+              appState.getNext();
+            },
+            child: Text('Next'),
+          ),
+          ] 
+          + myBoard(appState),
+      ),
+    );
+  }
+
+  List<Widget> myBoard(MyAppState appState) {
+    // out is the list of row widgets that make up the board. 
+    List<Widget> out = [];
+    for (int i=0; i<appState.board.length; i++){
+      // row is a list of strings
+      var row = appState.board[i];
+      List<Widget> rowView = [];
+      for (int j=0; j<row.length; j++) {
+        Color color;
+        switch ((i+j)%2){
+          case 0:
+            color=appState.white;
+            break;
+          default: 
+            color=appState.black;
+        }
+        var pieceCode = row[j];
+        rowView.add(
+          Square(pieceCode: pieceCode, color: color)
+        );
+      } 
+      out.add(Row(children:rowView, mainAxisAlignment: MainAxisAlignment.center,));
+    }
+    return out;
+  }
+}
+
+class Square extends StatelessWidget {
+  Square({
+    super.key,
+    required this.pieceCode,
+    required this.color,
+  });
+
+  final String pieceCode;
+  final Color color;
+  
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    Size screenSize = MediaQuery.of(context).size;
+    double screenWidth = screenSize.width;
+    double screenHeight = screenSize.height;
+    double squareW = (screenWidth-20)/8;
+
+    ButtonStyle style = ElevatedButton.styleFrom(
+      backgroundColor: color,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(1),
+      ),
+      );
+    String iconLoc = '';
+    if (pieceCode != Space) {
+      iconLoc = 'images/' + (imageMap[pieceCode] ?? '');
+    }
+
+    if (iconLoc=='') {
+      return SizedBox(
+          width: squareW,
+          height: squareW,
+          child: ElevatedButton(
+            style:style,
+            onPressed: () {
+              appState.getNext();
+            }, 
+            child: Text('')
+          ),
+        );
+    }
+    return SizedBox(
+        width: squareW,
+        height: squareW,
+        child: IconButton(
+          style:style,
+          icon: Image.asset(iconLoc),
+          onPressed: () {
+            appState.getNext();
+          },
+          
+        )
+      );
+  }
+}
+
+
