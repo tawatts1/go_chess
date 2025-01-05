1e07bbad8952c415b792f1dc21dbac6140fd935c
Thaddeus Watts <tedawatts@gmail.com>
2025-01-05 15:36:46 -0700

    Added naive king moves

--- chess_ffigen/src/board/moves.go
+++ chess_ffigen/src/board/moves.go
@@ -66,6 +66,12 @@ func (b board) GetMoves(c coord, bcm, wcm map[coord]bool) []move {
 		} else {
 			return b.GetKnightMoves(wcm, bcm, c)
 		}
+	} else if IsKing(piece) {
+		if blk {
+			return b.GetKingMoves(bcm, wcm, c)
+		} else {
+			return b.GetKingMoves(wcm, bcm, c)
+		}
 	} else {
 		panic("Not implemented")
 	}
@@ -199,6 +205,23 @@ func (b board) GetKnightMoves(friends, enemies map[coord]bool, c coord) []move {
 	var newSquare coord
 	for _, sign := range [2]int{-1, 1} {
 		for _, vector := range [4]coord{{y: 1, x: 2}, {y: 2, x: 1}, {y: -1, x: 2}, {y: -2, x: 1}} {
+			newSquare = c.Add(sign*vector.y, sign*vector.x)
+			if newSquare.IsInBoard() {
+				_, hasFriend := friends[newSquare]
+				if !hasFriend {
+					out = append(out, move{a: c, b: newSquare})
+				}
+			}
+		}
+	}
+	return out
+}
+
+func (b board) GetKingMoves(friends, enemies map[coord]bool, c coord) []move {
+	out := make([]move, 0, 2)
+	var newSquare coord
+	for _, sign := range [2]int{-1, 1} {
+		for _, vector := range [4]coord{{y: 1, x: 1}, {y: 1, x: 0}, {y: 0, x: 1}, {y: -1, x: 1}} {
 			newSquare = c.Add(sign*vector.y, sign*vector.x)
 			if newSquare.IsInBoard() {
 				_, hasFriend := friends[newSquare]
