081b20dad1d563c76dc2f8151ba1a6f4a3fb8b6b
Thaddeus Watts <tedawatts@gmail.com>
2025-01-08 11:32:31 -0700

    Moved constants to a dedicated constants file

---
+++ play/lib/constants.dart
@@ -0,0 +1,60 @@
+
+const piecesList = [
+  '0',
+  'p',
+  'n',
+  'b',
+  'r',
+  'q',
+  'k',
+  'o',
+  'a',
+  'P',
+  'N',
+  'B',
+  'R',
+  'Q',
+  'K',
+  'O',
+  'A'
+];
+
+const Space = '0';
+
+const BlackPawn = 'p';
+const BlackKnight = 'n';
+const BlackBishop = 'b';
+const BlackRookNC = 'r';
+const BlackQueen = 'q';
+const BlackKing = 'k';
+const BlackRookC = 'o';
+const BlackPawnEP = 'a';
+
+const WhitePawn =  'P';
+const WhiteKnight =  'N';
+const WhiteBishop =  'B';
+const WhiteRookNC =  'R';
+const WhiteQueen =  'Q';
+const WhiteKing =  'K';
+const WhiteRookC =  'O';
+const WhitePawnEP =  'A';
+
+
+const imageMap = {
+  BlackPawn: 'bp.png',
+  BlackKnight: 'bn.png',
+  BlackBishop: 'bb.png',
+  BlackRookNC: 'br.png',
+  BlackQueen: 'bq.png',
+  BlackKing: 'bk.png',
+  BlackRookC: 'br.png',
+  BlackPawnEP: 'bp.png',
+  WhitePawn: 'wp.png',
+  WhiteKnight: 'wn.png',
+  WhiteBishop: 'wb.png',
+  WhiteRookNC: 'wr.png',
+  WhiteQueen: 'wq.png',
+  WhiteKing: 'wk.png',
+  WhiteRookC: 'wr.png',
+  WhitePawnEP: 'wp.png'
+};
