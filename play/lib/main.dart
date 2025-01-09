7c90cfc02d784513055b3d7f44862d795c7edb1d
Thaddeus Watts <tedawatts@gmail.com>
2025-01-08 19:31:20 -0700

    removed unused import

--- play/lib/main.dart
+++ play/lib/main.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:ffi/ffi.dart';
 import 'constants.dart';
-import 'coordinate.dart';
 
 import 'package:go_chess/go_chess.dart' as go_chess;
 
