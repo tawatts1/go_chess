539d709f0843dbe35dec76dffee092851eb01306
Thaddeus Watts <tedawatts@gmail.com>
2025-01-12 13:40:30 -0700

    Made ai more random in case there are many moves of the same score

--- chess_ffigen/src/ai/ai.go
+++ chess_ffigen/src/ai/ai.go
@@ -1,9 +1,13 @@
 package ai
 
 import (
+	"math"
+
 	"github.com/tawatts1/go_chess/board"
 	"golang.org/x/exp/rand"
 )
+
+const Epsilon = 0.0001
 
 var simpleAICode = "simple"
 
@@ -37,17 +41,26 @@ func newMoveList(moves []board.Move) moveList {
 	return moveList{moves: moves, scores: make([]float64, len(moves), len(moves)), size: len(moves)}
 }
 
+func IsClose(num1, num2 float64) bool {
+	return math.Abs(num1-num2) < Epsilon
+}
+
+// after the scores are calculated, choose the best move. No need for this function to be efficient as it is called only once.
 func (mList moveList) GetMaxScoreMove() board.Move {
 	if mList.size > 0 {
 		maxScore := mList.scores[0]
-		maxScoreMove := mList.moves[0]
 		for i := 1; i < mList.size; i++ {
 			if mList.scores[i] > maxScore {
 				maxScore = mList.scores[i]
-				maxScoreMove = mList.moves[i]
 			}
 		}
-		return maxScoreMove
+		bestMoves := make([]board.Move, 0)
+		for i := 0; i < mList.size; i++ {
+			if IsClose(maxScore, mList.scores[i]) {
+				bestMoves = append(bestMoves, mList.moves[i])
+			}
+		}
+		return bestMoves[rand.Intn(len(bestMoves))]
 	} else {
 		panic("No moves")
 	}
