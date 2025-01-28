b3bfac45582c4c2c193f02bcc99e10307afdda0f
Thaddeus Watts <tedawatts@gmail.com>
2025-01-28 09:10:08 -0700

    updated ai tests and added comments, updated verbosity usage in testing

--- chess_ffigen/src/ai/ai_test.go
+++ chess_ffigen/src/ai/ai_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/tawatts1/go_chess/utility"
 )
 
-var verbosity int = 2
+var verbosity int = 0 // 0 - nothing, 1 - just input lines, 2 - everything
 var testFolder = "testingMoves/"
 
 func check(e error) {
@@ -42,22 +42,17 @@ func testAiMoveFile(fname string) string {
 		if args[0] == "new" {
 			//a = GetAiFromString(args[1])
 
-			Nparsed, ok := utility.StrToInt(args[2])
-			b = board.GetBoardFromString(args[3])
+			b = board.GetBoardFromString(args[2])
 			if verbosity > 1 {
 				fmt.Println(b)
 			}
-			if hasError(ok) {
-				panic("failed to parse N")
-			} else {
-				N = Nparsed
-			}
 		} else if args[0] == "move" || args[0] == "notmove" {
-			color := args[1]
-			y1, ok1 := utility.StrToInt(args[2])
-			x1, ok2 := utility.StrToInt(args[3])
-			y2, ok3 := utility.StrToInt(args[4])
-			x2, ok4 := utility.StrToInt(args[5])
+			Nparsed, okN := utility.StrToInt(args[1])
+			color := args[2]
+			y1, ok1 := utility.StrToInt(args[3])
+			x1, ok2 := utility.StrToInt(args[4])
+			y2, ok3 := utility.StrToInt(args[5])
+			x2, ok4 := utility.StrToInt(args[6])
 			var isWhite bool
 			if color == string(board.White) {
 				isWhite = true
@@ -68,14 +63,15 @@ func testAiMoveFile(fname string) string {
 			}
 			var special rune
 			var c1, c2 board.Coord
-			if hasError(ok1) || hasError(ok2) || hasError(ok3) || hasError(ok4) {
+			if hasError(ok1) || hasError(ok2) || hasError(ok3) || hasError(ok4) || hasError(okN) {
 				panic(fmt.Sprintf("failed to parse line %v", lineIndex+1))
 			} else {
 				c1 = board.NewCoord(y1, x1)
 				c2 = board.NewCoord(y2, x2)
+				N = Nparsed
 			}
 
-			r := []rune(strings.TrimSpace(args[6]))
+			r := []rune(strings.TrimSpace(args[7]))
 			if len(r) > 1 {
 				return fmt.Sprintf("%v: special column must be one or zero characters. ", lineIndex+1)
 			} else if len(r) == 1 {
@@ -87,6 +83,11 @@ func testAiMoveFile(fname string) string {
 				return fmt.Sprintf("line %v: Expected %v but got %v", lineIndex+1, mExpected, mResult)
 			} else if args[0] == "notmove" && mResult.Equals(mExpected) {
 				return fmt.Sprintf("line %v: Expected anything but %v", lineIndex+1, mResult)
+			}
+		} else if args[0] == "verbosity" {
+			v, vok := utility.StrToInt(args[1])
+			if !hasError(vok) {
+				verbosity = v
 			}
 		}
 	}
