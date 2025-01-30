63a26eca90dda765f387e47b7db04d68fac6abfb
Thaddeus Watts <tedawatts@gmail.com>
2025-01-30 12:59:45 -0700

    Added comments and renamed function

--- chess_ffigen/src/ai/ai.go
+++ chess_ffigen/src/ai/ai.go
@@ -56,19 +56,21 @@ func (mList moveList) GetMaxScoreMove() board.Move {
 	}
 }
 
+// Calculate moves and their scores and return one of the moves with the max score
 func ChooseMove(b board.Board, isWhite bool, depth int) board.Move {
 	mList := newMoveList(b.GetLegalMoves(isWhite))
-	mList = SortMoveList(mList, b, isWhite, depth)
+	mList = ScoreSortMoveList(mList, b, isWhite, depth)
 	return mList.GetMaxScoreMove()
 }
 
-func SortMoveList(mList moveList, b board.Board, isWhite bool, depth int) moveList {
+// Score and sort the move list.
+func ScoreSortMoveList(mList moveList, b board.Board, isWhite bool, depth int) moveList {
 	// calculate score of moves for a certain depth, then return sorted moveList
 	if depth == 0 {
 		return mList
 	} else if depth > 0 {
 		if depth > 1 {
-			mList = SortMoveList(mList, b, isWhite, depth-1)
+			mList = ScoreSortMoveList(mList, b, isWhite, depth-1)
 		}
 		wcs := -utility.Infinity
 		for i := range mList.size {
@@ -112,6 +114,7 @@ func (mList moveList) isSortedDesc() bool {
 	return true
 }
 
+// Get the score by looking 'depth' number of moves ahead.
 func GetScore(b board.Board, isWhite bool, depth int, parent_wcs float64) float64 {
 	wcs := -utility.Infinity
 	if depth == 0 {
