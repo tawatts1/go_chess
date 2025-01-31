33a7db9ba27adb4718b62091f4fd224ef72eb8a3
Thaddeus Watts <tedawatts@gmail.com>
2025-01-31 13:33:56 -0700

    Fixed bug with position scoring and reran benchmarks

--- chess_ffigen/src/ai/boardScore.go
+++ chess_ffigen/src/ai/boardScore.go
@@ -23,7 +23,7 @@ func getScoringFuncFromPieceValueMap(pieceValueMap map[rune]float64) func(board.
 	return func(b board.Board, isWhite bool) float64 {
 		var out float64
 		var multiplier float64 = 1
-		if isWhite {
+		if !isWhite {
 			multiplier = -1
 		}
 		var p rune
@@ -37,51 +37,64 @@ func getScoringFuncFromPieceValueMap(pieceValueMap map[rune]float64) func(board.
 	}
 }
 
-func getPositionScoreValue(b board.Board, isWhite bool) float64 {
+func getPositionScoreValue(b board.Board, isWhitesMove bool) float64 {
 	var out float64
-	var multiplier float64 = 1
-	if isWhite {
-		multiplier = -1
+	var moveColorMultiplier float64 = 1
+	if !isWhitesMove {
+		moveColorMultiplier = -1
 	}
 	var p rune
+	var pieceColorMultiplier float64 = 1
 	for _, c := range board.AllCoordinates {
 		p = b.GetPiece(c)
+		if board.IsWhite(p) {
+			pieceColorMultiplier = 1
+		} else {
+			pieceColorMultiplier = -1
+		}
 		if p != board.Space {
 			//add the inherent value of the piece
-			out += multiplier * defaultPieceValue[p]
+			//the piece color multiplier is built into the map.
+			out += moveColorMultiplier * defaultPieceValue[p]
 			//calculate and add the value based on the pieces position
-			out += multiplier * positionScalingFactor * GetPositionScore(c, isWhite, p)
+			out += moveColorMultiplier * pieceColorMultiplier * positionScalingFactor * GetPositionScore(c, p)
 		}
 	}
 	return out
 }
 
-func GetPositionScore(c board.Coord, isWhite bool, p rune) float64 {
+// Returns a positive value no matter the piece color.
+func GetPositionScore(c board.Coord, p rune) float64 {
+	var out float64
 	if board.IsPawn(p) {
 		//Pawns are more valuable if they are close to promotion.
 		y := c.GetY()
-		if isWhite {
+		if board.IsWhite(p) {
 			y = 7 - y // normalize for color
 		}
 		if y == 1 {
-			return 0.5
+			out = 0.5
 		} else {
-			return float64(y - 2)
+			out = float64(y - 2)
 		}
 
 	} else if board.IsBishop(p) || board.IsQueen(p) {
 		y, x := c.GetY(), c.GetX()
 		min, max := minimum(y, x), -minimum(-y, -x)
 		distToEdge := minimum(7-max, min)
-		out := float64(distToEdge) * 0.5
+		out = float64(distToEdge) * 0.5
 		if (y == x || 7-y == x) && distToEdge != 3 {
 			//favor the diagonals for bishops/queens
 			out += 0.5
 		}
-
-		return out
 	} else if board.IsKnight(p) {
 		y, x := c.GetY(), c.GetX()
+		isInHomePosition := false
+		if y == 0 || y == 7 {
+			if x == 1 || x == 6 {
+				isInHomePosition = true
+			}
+		}
 		//flip x and y to upper left corner of board.
 		if y > 3 {
 			y = 7 - y
@@ -89,7 +102,6 @@ func GetPositionScore(c board.Coord, isWhite bool, p rune) float64 {
 		if x > 3 {
 			x = 7 - x
 		}
-		var out float64
 		if y > 1 && x > 1 {
 			out = 1
 		} else if y > 1 || x > 1 {
@@ -99,12 +111,19 @@ func GetPositionScore(c board.Coord, isWhite bool, p rune) float64 {
 				out = .75
 			}
 		} else {
-			out = float64(x+y+2) / 8.0
+			if isInHomePosition {
+				out = .5
+			} else {
+				out = float64(x+y+2) / 8.0
+			}
 		}
-		return out
-	} else {
-		return 0
 	}
+	// if board.IsWhite(p) {
+	// 	return out
+	// } else {
+	// 	return out * -1
+	// }
+	return out
 }
 
 func minimum(a, b int) int {
@@ -116,41 +135,41 @@ func minimum(a, b int) int {
 }
 
 var defaultPieceValue = map[rune]float64{
-	board.BlackPawn:   1,
-	board.BlackKnight: 2.6,
-	board.BlackBishop: 3,
-	board.BlackRookNC: 4.99,
-	board.BlackQueen:  9,
+	board.BlackPawn:   -1,
+	board.BlackKnight: -2.6,
+	board.BlackBishop: -3,
+	board.BlackRookNC: -4.99,
+	board.BlackQueen:  -9,
 	board.BlackKing:   0,
-	board.BlackRookC:  5, // a rook with the ability to castle
-	board.BlackPawnEP: 1, // a pawn which can be taken via en passant
+	board.BlackRookC:  -5, // a rook with the ability to castle
+	board.BlackPawnEP: -1, // a pawn which can be taken via en passant
 	//--------
-	board.WhitePawn:   -1,
-	board.WhiteKnight: -2.6,
-	board.WhiteBishop: -3,
-	board.WhiteRookNC: -4.99,
-	board.WhiteQueen:  -9,
+	board.WhitePawn:   1,
+	board.WhiteKnight: 2.6,
+	board.WhiteBishop: 3,
+	board.WhiteRookNC: 4.99,
+	board.WhiteQueen:  9,
 	board.WhiteKing:   0,
-	board.WhiteRookC:  -5, // a rook with the ability to castle
-	board.WhitePawnEP: -1, // a pawn which can be taken via en passant
+	board.WhiteRookC:  5, // a rook with the ability to castle
+	board.WhitePawnEP: 1, // a pawn which can be taken via en passant
 }
 
 var highKnightPieceValue = map[rune]float64{
-	board.BlackPawn:   1,
-	board.BlackKnight: 3.3,
-	board.BlackBishop: 3,
-	board.BlackRookNC: 4.99,
-	board.BlackQueen:  9,
+	board.BlackPawn:   -1,
+	board.BlackKnight: -3.3,
+	board.BlackBishop: -3,
+	board.BlackRookNC: -4.99,
+	board.BlackQueen:  -9,
 	board.BlackKing:   0,
-	board.BlackRookC:  5, // a rook with the ability to castle
-	board.BlackPawnEP: 1, // a pawn which can be taken via en passant
+	board.BlackRookC:  -5, // a rook with the ability to castle
+	board.BlackPawnEP: -1, // a pawn which can be taken via en passant
 	//--------
-	board.WhitePawn:   -1,
-	board.WhiteKnight: -3.3,
-	board.WhiteBishop: -3,
-	board.WhiteRookNC: -4.99,
-	board.WhiteQueen:  -9,
+	board.WhitePawn:   1,
+	board.WhiteKnight: 3.3,
+	board.WhiteBishop: 3,
+	board.WhiteRookNC: 4.99,
+	board.WhiteQueen:  9,
 	board.WhiteKing:   0,
-	board.WhiteRookC:  -5, // a rook with the ability to castle
-	board.WhitePawnEP: -1, // a pawn which can be taken via en passant
+	board.WhiteRookC:  5, // a rook with the ability to castle
+	board.WhitePawnEP: 1, // a pawn which can be taken via en passant
 }
